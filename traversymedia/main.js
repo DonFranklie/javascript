f81e29b55f6049717d8271d1c9fecb5f30aa7505
DonFranklie <107101640+DonFranklie@users.noreply.github.com>
2022-11-11 16:33:44 +0300

    Variables

---
+++ traversymedia/main.js
@@ -0,0 +1,10 @@
+// Variables
+// let and const
+
+// With let- you can reassign values, with const you can't
+
+let age = 30;
+
+// age = 40;
+
+// Const is used when only assigning values that are not going to be changed in the future
