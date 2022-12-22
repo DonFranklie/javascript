8d75a8d3aa5a874b302f12ec9aaa2293007994a4
DonFranklie <107101640+DonFranklie@users.noreply.github.com>
2022-12-22 08:53:02 +0300

    Methods and properties

--- traversymedia/main.js
+++ traversymedia/main.js
@@ -1,6 +1,12 @@
+const name = 'Franklin, Makokha, Wafula';
 
+console.log(name.length) // LEngth is a property so it does not need parentheses
 
+console.log(name.toUpperCase()) // toUpperCase is a method so it  needs parentheses
 
+const isArr = name.split(','); // LEngth is a property so it does not need parentheses
+
+console.log(isArr)
 
 
 
