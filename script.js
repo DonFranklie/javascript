b3fe49a0686dbc9c76a1b9d55c5d82bf7441011c
Don Franklie <donfranklie@gmail.com>
2023-07-17 14:55:40 +0300

    Js Tutorial

---
+++ script.js
@@ -0,0 +1,19 @@
+// "use strict";
+var name = "";
+
+switch(name){
+case 'Amos':
+    document.body.innerHTML = 'Amos'; 
+    break;
+case 'Martha':
+    document.body.innerHTML = 'Martha'; 
+    break;
+case 'Franklin':
+    document.body.innerHTML = 'Franklin'; 
+    break;
+
+    default:
+    document.body.innerHTML = 'No name given'; 
+    break;
+
+}
