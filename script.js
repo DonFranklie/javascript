86d17914d451674eba0c02cb997ed15ba8753560
DonFranklie <donfranklie@gmail.com>
2023-07-30 10:22:13 +0300

    Script: Type conversion

---
+++ script.js
@@ -0,0 +1,28 @@
+// How to convert a value to a string.
+// let value = 12345;
+
+// value = String(value);
+
+// alert(value);
+
+// How to convert a value to a Number
+
+// let str = "24";
+
+// let num = Number(str);
+
+// alert(typeof str);
+
+// alert(Number(true)); //Outputs 1
+// alert(Number(false)); //Outputs 0
+
+// let message = null;
+
+// alert(Number(message));
+
+// Boolean Conversion
+
+// alert(Boolean("")); // Outputs false
+// alert(Boolean(" ")); // With spaces, outputs true
+
+alert(Boolean("123"));
