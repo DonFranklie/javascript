6ebee082982d024315be06af1c7fe98bfd199f41
DonFranklie <donfranklie@gmail.com>
2023-08-08 07:26:47 +0300

    Functions: Return, naming

--- script.js
+++ script.js
@@ -1,39 +1,80 @@
 // FUNCTIONS
-// Default values
-// These are what are given when an argument is not passed into a function when it's being called.
+// Naming functions
+// A function shold be namd according to what it does, this is why
+// we should use verbal prefixes to explicitly describe a function's usage.
+// And a function should only be able to do the action described by it's name, no more no less
+// For examples:
+// changeColor is function that changes the color of something, let that be only action it does.
 
-// let from = "Franklin";
-// function showMessage(from, text = "empty message"){ 
-//     from = "*"+from+"*";
-//     alert(from+": " +text);
+// Returning a Value
+// A function can return a value back into the calling code as the result.
+
+// function sum(a,b){
+//     return a + b;
 // }
 
-// showMessage(from); 
+// alert(sum(4,3));
+
+// function checkAge(age){
+//     if(age>=18){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// };
+
+// function checkAge(age){age>18?true:confirm("Did Parents allow you?")};
+
+// function checkAge(age){ return( age>18||confirm("Did your parents allow you?"))}
+
+// let age = +prompt("How old are you? ", 18);
+// checkAge(age);
+
+// if(checkAge(age)){
+//     alert("Access allowed");
+// }else{
+//     alert("Access denied");
+// }
+
+//Note
+//If an expression is long after the return directive, don't go to the next line unless
+// you wrap the expression as a return value like this:
+
+// if(money){
+// return (
+//     some + long + expression + (a * b)
+// );
+// };
 
 
-let text = prompt("What is your text? ", '');
-function showMessage(text){
-    // text = text ;
-    alert(text|| "No text given");
+// function min(a, b){
+//     if(a>b){
+//         return alert(b)
+//     }else if(b>a){
+//         return alert(a)
+//     }else{
+//         return alert("They are equal");
+//     }
+// };
+
+// min(3,3);
+
+function pow(x, n){
+    return x**n;
+
 }
 
-showMessage(text);
+let x = +prompt("Enter x:", 0);
+let n = +prompt("Enter n:", 1);
 
 
+if(x<1){
+    alert(`${x} is a not natural number`)
+}else if(n<1){
+    alert(`${n} is a not natural number`)
+}
+else{
+pow(x, n);
+}
 
-
-
-// Function Parameters
-
-// let from = "Franklin";
-// function showMessage(from, text){ 
-//     //The ones inside the parentheses when the function is declared are called parameters
-//     from = "*"+from+"*";
-//     alert(from+": " +text);
-// }
-
-// showMessage(from, "Hello guys"); 
-// // The ones inside the parentheses when the function is called are called arguments
-
-// alert(from);
-
+alert(pow(x,n));
